928034101e1fb64ff8ea2c72fd7b319e0378f541
MohamedMahmoudddd <oisbha626ytt@gmail.com>
2026-08-17 16:51:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+namespace day02
+{
+    class person
+    {
+        public int x;
+        public int y;
+    }
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            #region problem 1
+            // this program calculate sum of 2 numbers
+            /*this program calculate sum of 2 numbers*/
+            //int x = 10;
+            //int y = 20;
+            //int sum = x + y;
+            //Console.WriteLine(sum);
+            #endregion
+
+            #region Q1
+            //cntrl + k c
+            //cntrl + k u
+            #endregion
+
+            #region problem 2
+            //int x = 10;
+            //int y = 5;
+            //Console.WriteLine(x+y);
+            #endregion
+
+            #region Q2
+            //logic error 
+            //3+3 =9
+            //run time
+            //4/0
+            #endregion
+
+            #region problem 3
+            //Console.WriteLine("enter ur name");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("enter ur age");
+            //int age = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("enter ur salary");
+            //decimal salary = decimal.Parse(Console.ReadLine());
+
+            //Console.WriteLine("are you student");
+            //bool student =bool.Parse(Console.ReadLine());
+
+            //Console.WriteLine("your name " + name);
+            //Console.WriteLine("your age " + age);
+            //Console.WriteLine("your salary " + salary);
+            #endregion
+
+            #region Q3
+            // make code easier to read and understand for every one
+            #endregion
+
+            #region problem 4
+
+            //person p1;
+            //p1 = new person();
+            //p1.x = 10;
+            //p1.y = 20;
+            //person p2 = new person();
+            //p2 = p1;
+            //Console.WriteLine(p1.x);
+            //Console.WriteLine(p2.x);
+            #endregion
+
+            #region Q4
+            // value 
+            // stored in stack
+            // static size
+            //ref
+            // stored in heap 
+            // dynamic size
+            #endregion
+            #region Problem 5
+
+            //int x = 15;
+            //int y = 4;
+
+
+            //int sum = x + y;
+            //int difference = x - y;   
+            //int product = x * y;     
+            //int division = x / y;   
+            //int remainder = x % y;
+            //Console.WriteLine(sum);
+            //Console.WriteLine(difference);
+            //Console.WriteLine(product);
+            //Console.WriteLine(remainder);
+            #endregion
+
+            //#region Q5
+            //int a = 2, b = 7;
+            //Console.WriteLine(a % b);//2 bec a is smaller than b and a/b =0
+            //#endregion
+
+            #region problem 6
+            //int z = 5;
+            //string res = (z % 2 == 0 && z > 10) ? "Even and greter than 10" : "not condition";
+            //Console.WriteLine(res);
+            #endregion
+
+            #region Q5
+            //&& short circut if first cond is false not see other condition
+            //& long circut if first cond it see other cond
+            #endregion
+
+            #region problem 7
+            //double c = 98585.985;
+            //int v;
+            //checked
+            //{
+            //    v = (int)c; ;
+            //}
+            //Console.WriteLine(v);
+            #endregion
+
+            #region Q6
+            // bec double is larger than int so we use explicit 
+            // if we use implicit it will happen loss data
+            #endregion
+
+            #region problem 8
+            //Console.WriteLine("enter ur age");
+            //int age =int.Parse (Console.ReadLine());
+            //string s  = age > 0 ? "valid" : "not valid";
+            //Console.WriteLine(s);
+            #endregion
+
+            #region Q7
+            // formate exception to solve use try prase or use try and catch
+            #endregion
+
+            #region problem 9
+            //int x = 6;
+            //Console.WriteLine(x++);
+            //int y = 6;
+            //Console.WriteLine(++y);
+            #endregion
+
+            #region Q8
+            //int x = 5;
+            //int y = ++x + x++;//12 & x = 7
+            #endregion
+        }
+    }
+}
